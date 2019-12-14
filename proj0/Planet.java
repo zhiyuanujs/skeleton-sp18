2d606b261ef36e1793902a2589cdaf7ce144fa54
Xu <zhiyuanoutlook.com>
2019-12-14 22:08:06 +0100

    Create Planet.java

---
+++ proj0/Planet.java
@@ -0,0 +1,28 @@
+public class Planet{
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	String imgFileName;
+	
+	public Planet(double xP, double yP, double xV, double yV, double m, String img){
+		
+		xxPos=xP;
+		yyPos=yP;
+		xxVel=xV;
+		yyVel=yV;
+		mass=m;
+		imgFileName=img;
+		
+	}
+	
+	public Planet(Planet p){
+		xxPos=p.xxPos;
+		yyPos=p.yyPos;
+		xxVel=p.xxVel;
+		yyVel=p.yyVel;
+		mass=p.mass;
+		imgFileName=p.imgFileName;
+	}
+}
